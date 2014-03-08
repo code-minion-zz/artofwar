16046240978e1dd91c2e3bfb4d1c213918c3c250
Kit <chanwk3@gmail.com>
2014-03-09 01:52:19 +1300

    HP Bar which follows enemies

---
+++ artofwar/Assets/Scripts/HPBarHider.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPBarHider : MonoBehaviour {
+
+	public UISprite sprite;
+	Transform player;
+	Transform owner;
+	bool initialized = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.Find("3rd Person Controller").transform;
+		owner = sprite.leftAnchor.target;
+		initialized = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!initialized) return;
+
+		float distance = Vector3.Distance(player.position, owner.position);
+		Debug.Log(distance);
+		if (distance > 20f)
+		{
+			if (sprite.gameObject.activeSelf)
+			{
+				NGUITools.SetActive(sprite.gameObject, false);
+			}
+		}
+		else// (distance < 10f)
+		{
+			if (!sprite.gameObject.activeSelf)
+			{
+				NGUITools.SetActive(sprite.gameObject, true);
+			}
+		}
+	}
+}
