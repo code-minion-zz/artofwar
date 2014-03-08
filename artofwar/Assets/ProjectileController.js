197648188515e32c143f09455d09179456c3f275
Lars Dobbertin-King <larztheloser@gmail.com>
2014-03-09 12:19:44 +1300

    Chicken Projectile
    Shoot!

--- artofwar/Assets/ProjectileController.js
+++ artofwar/Assets/ProjectileController.js
@@ -1,10 +1,11 @@
 ﻿#pragma strict
-
-@script RequireComponent(CharacterController)
-
-function Update () {
-	var controller : CharacterController = GetComponent(CharacterController);
-	var movement = Vector3(0,-1*Time.deltaTime,0);
-	controller.Move(movement);
-	transform.LookAt(Camera.main.transform.position, -Vector3.up);
+ 
+var _time:float = 3f;
+ 
+function Update() {
+	if(gameObject.name!="Chicken Clone") return;
+	var sprr = GetComponent(SpriteRenderer);
+	_time=_time-Time.deltaTime;
+	if(_time>0 && _time<1) sprr.color.a=_time;
+	if(_time<=0) Destroy(gameObject);
 }
--- artofwar/Assets/ProjectileLauncher.js
+++ artofwar/Assets/ProjectileLauncher.js
@@ -6,8 +6,11 @@ function Update () {
 	if(Input.GetKeyDown("space")) {
 		if(chickencount > 0) {
 			chickencount=chickencount-1;
-			Instantiate(GameObject.Find("Chicken Projectile"),transform.position + Vector3(0,3,0), Quaternion.identity);
-			
+			var newchicken=Instantiate(GameObject.Find("Chicken Projectile"),transform.position + Vector3(0,2,0), transform.rotation);
+			newchicken.rigidbody.AddForce(newchicken.transform.forward * 9000);
+			newchicken.rigidbody.AddForce(newchicken.transform.up * 500);
+			newchicken.rigidbody.AddForce(newchicken.transform.right * Random.Range(-100,100));
+			newchicken.name="Chicken Clone";
 		}
 	}
 }
