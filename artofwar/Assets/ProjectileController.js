fe61eaa8e1f811ab209e5dbffa71dbb1b16a52f2
Lars Dobbertin-King <larztheloser@gmail.com>
2014-03-09 01:48:46 +1300

    Chicken instances
    Poultry goodness

---
+++ artofwar/Assets/ProjectileController.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+@script RequireComponent(CharacterController)
+
+function Update () {
+	var controller : CharacterController = GetComponent(CharacterController);
+	var movement = Vector3(0,-1*Time.deltaTime,0);
+	controller.Move(movement);
+	transform.LookAt(Camera.main.transform.position, -Vector3.up);
+}
---
+++ artofwar/Assets/ProjectileLauncher.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+public var chickencount : int = 5;
+
+function Update () {
+	if(Input.GetKeyDown("space")) {
+		if(chickencount > 0) {
+			chickencount=chickencount-1;
+			Instantiate(GameObject.Find("Chicken Projectile"),transform.position + Vector3(0,3,0), Quaternion.identity);
+			
+		}
+	}
+}
