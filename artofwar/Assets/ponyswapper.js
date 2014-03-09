aa7ee3959e4abb5f740621a74455451d2b064c1e
Lars Dobbertin-King <larztheloser@gmail.com>
2014-03-09 18:48:29 +1300

    More ponies
    Because one is never enough

---
+++ artofwar/Assets/ponyswapper.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) {
+		GameObject.Find("ab").GetComponent(MeshFilter).mesh=Resources.Load("ab", Mesh);
+		GameObject.Find("ab").GetComponent(MeshRenderer).material.mainTexture = Resources.Load("Materials/cylinder", Texture2D);
+	}
+	if(Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)) {
+		GameObject.Find("ab").GetComponent(MeshFilter).mesh=Resources.Load("sb", Mesh);
+		GameObject.Find("ab").GetComponent(MeshRenderer).material.mainTexture = Resources.Load("Materials/cylinder 1", Texture2D);
+	}
+}
